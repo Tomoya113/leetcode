2faa42765ed872dafe5ea9314587dfd1881e980d
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-09-26 23:57:24 +0900

    done 001

---
+++ 001-Two-Sum/001.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let result = []
+
+  
+  for(let i=0; i<nums.length; i++) {
+      result[0] = i
+      for(let i=0; i<nums.length; i++) {
+          if (target - nums[result[0]] == nums[i] ) {
+              result[1] = i
+              break;
+          }
+      }
+      if (result.length == 2) {break;}
+  }
+  return result;
+};
