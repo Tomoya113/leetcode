25d734d067e8a08a5833dcf53e6721ad9a2cd242
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-09-27 22:29:48 +0900

    javascirptを使った少数の計算は色々だるい

--- 007-Reverse-Integer/007.js
+++ 007-Reverse-Integer/007.js
@@ -18,3 +18,15 @@ var reverse = function(x) {
           }
       }   
 };
+
+
+// こっちのほうがきれいっすね
+var reverse = function(x) {
+    
+  const reversedInt = parseInt(Math.abs(x).toString().split('').reverse().join(''));
+  
+  if (reversedInt > 2**31) return 0;
+  
+  return reversedInt * Math.sign(x);
+    
+};
